0b044269d6e826cb6d2fc3a506be45c63e2fd21d
lopun <ujin43255252@gmail.com>
2018-10-18 20:41:21 +0900

    Verification api & entity added / shared types configured

---
+++ src/entities/Verification.ts
@@ -0,0 +1,48 @@
+import {
+  Entity,
+  BaseEntity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  BeforeInsert
+} from "typeorm";
+import { verificationTarget } from "src/types/types";
+
+const PHONE = "PHONE";
+const EMAIL = "EMAIL";
+
+@Entity()
+class Verification extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: "text", enum: [PHONE, EMAIL] })
+  target: verificationTarget;
+
+  @Column({ type: "text" })
+  payload: string;
+
+  @Column({ type: "text" })
+  key: string;
+
+  @Column({ type: "boolean", default: false })
+  used: boolean;
+
+  @CreateDateColumn()
+  createdAt: string;
+  @CreateDateColumn()
+  updatedAt: string;
+
+  @BeforeInsert()
+  createKey(): void {
+    if (this.target === PHONE) {
+      this.key = Math.floor(Math.random() * 10000).toString();
+    } else if (this.target === EMAIL) {
+      this.key = Math.random()
+        .toString(36)
+        .substr(2);
+    }
+  }
+}
+
+export default Verification;
