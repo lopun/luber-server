806a1bf2293722dedc1f4658b3d148f9b353f531
lopun <ujin43255252@gmail.com>
2018-10-20 13:58:48 +0900

    Driver Report Movement & Subscription added

--- src/api/User/DriversSubscription/DriversSubscription.resolvers.ts
+++ src/api/User/DriversSubscription/DriversSubscription.resolvers.ts
@@ -1,9 +1,26 @@
+import { withFilter } from "apollo-server-express";
+import User from "entities/User";
+
 const resolvers = {
   Subscription: {
     DriversSubscription: {
-      subscribe: (_, __, { pubSub }) => {
-        return pubSub.asyncIterator("driverUpdate");
-      }
+      subscribe: withFilter(
+        (_, __, { pubSub }) => pubSub.asyncIterator("driverUpdate"),
+        (payload, _, { context }) => {
+          const user: User = context.currentUser;
+          const {
+            DriversSubscription: {
+              lastLat: driverLastLat,
+              lastLng: driverLastLng
+            }
+          } = payload;
+          const { lastLat: userLastLat, lastLng: userLastLng } = user;
+          return (
+            Math.abs(driverLastLat - userLastLat) <= 0.05 &&
+            Math.abs(driverLastLng - userLastLng) <= 0.05
+          );
+        }
+      )
     }
   }
 };
