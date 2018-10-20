a4e66a84fabcc617dd6a756601e55e75a33a5a50
lopun <ujin43255252@gmail.com>
2018-10-20 14:41:49 +0900

    GetNearbyRides => Ride

--- src/api/Ride/GetNearbyRide/GetNearbyRide.resolvers.ts
+++ src/api/Ride/GetNearbyRide/GetNearbyRide.resolvers.ts
@@ -1,40 +1,48 @@
 import { Resolvers } from "types/resolvers";
 import privateResolver from "utils/privateResolver";
-import { GetNearbyRidesResponse } from "types/graph";
+import { GetNearbyRideResponse } from "types/graph";
 import User from "entities/User";
 import Ride from "entities/Ride";
 import { getRepository, Between } from "typeorm";
 
 const resolvers: Resolvers = {
   Mutation: {
-    GetNearbyRides: privateResolver(
-      async (_, __, { req }): Promise<GetNearbyRidesResponse> => {
+    GetNearbyRide: privateResolver(
+      async (_, __, { req }): Promise<GetNearbyRideResponse> => {
         const user: User = req.user;
         if (user.isDriving) {
           const { lastLat, lastLng } = user;
           try {
-            const rides = await getRepository(Ride).find({
+            const ride = await getRepository(Ride).findOne({
               status: "REQUESTING",
               pickUpLat: Between(lastLat - 0.05, lastLat + 0.05),
               pickUpLng: Between(lastLng - 0.05, lastLng + 0.05)
             });
-            return {
-              ok: true,
-              error: null,
-              rides
-            };
+            if (ride) {
+              return {
+                ok: true,
+                error: null,
+                ride
+              };
+            } else {
+              return {
+                ok: true,
+                error: "There is no request for a ride.",
+                ride: null
+              };
+            }
           } catch (error) {
             return {
               ok: false,
               error: error.message,
-              rides: null
+              ride: null
             };
           }
         } else {
           return {
             ok: false,
             error: "You are not driving",
-            rides: null
+            ride: null
           };
         }
       }
